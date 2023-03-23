8bd5bae5f2d3740b982bd7aac1f97cde67952c12
Quentin LE NESTOUR <quentin.le_nestour@edu.devinci.fr>
2023-03-23 12:16:11 +0100

    --

--- python S6.py
+++ python S6.py
@@ -21,11 +21,14 @@ class Othello :
         
     
     def findSandwiches(self, position):
+        self.sandwiches = []
         difs = [(-2,0), (2,0),(0,-2),(0,2),(-2,-2),(-2,2),(2,-2),(2,2)]
         isSandwich = False
         for dif in difs:
-            if(self.gameBoard[position[0]+dif[0], position[1]+dif[1]] == self.player
-               and self.gameBoard[position[0]+dif[0]/2, position[1]+dif[1]/2] == self.player*-1): 
+            if(position[0]+dif[0] < 8 and position[1]+dif[1] < 8
+                and self.gameBoard[position[0]+dif[0], position[1]+dif[1]] == self.player
+                and self.gameBoard[position[0]+dif[0]//2, position[1]+dif[1]//2] == self.player*-1): 
+
                 isSandwich = True
                 self.sandwiches.append(dif)
             
@@ -34,24 +37,26 @@ class Othello :
     
     def positionValid(self, position):
         
-        return (
-            self.gameBoard[position[0], position[1]] == 0
-            and(
-                self.gameBoard[position[0]-1, position[1]] == self.player*-1
-                or self.gameBoard[position[0]+1, position[1]] == self.player*-1
-                or self.gameBoard[position[0], position[1]-1] == self.player*-1
-                or self.gameBoard[position[0], position[1]+1] == self.player*-1
-                )
-            and(
-                self.findSandwiches(position)
-                )
-            
+        condition1 = self.gameBoard[position[0], position[1]] == 0
+        condition2 = (
+            (position[0]-1 >= 0 and self.gameBoard[position[0]-1, position[1]] == self.player*-1)
+            or (position[0]+1 < 8 and self.gameBoard[position[0]+1, position[1]] == self.player*-1)
+            or (position[1]-1 >= 0 and self.gameBoard[position[0], position[1]-1] == self.player*-1)
+            or (position[1]+1 < 8 and self.gameBoard[position[0], position[1]+1] == self.player*-1)
             )
+           
+        condition3 = self.findSandwiches(position)
+                
+
+        return condition1 and condition2 and condition3
+            
+            
+            
             
     def updateGameBoard(self, position):
         self.gameBoard[position[0], position[1]] = self.player
         for diff in self.sandwiches:
-            self.gameBoard[position[0]-diff[0]/2, position[1] - diff[1]/2] = self.player
+            self.gameBoard[position[0] + diff[0]//2, position[1] + diff[1]//2] = self.player
                 
             
     def showConsoleGameBoard(self):
@@ -61,40 +66,58 @@ class Othello :
             print(" "+str(i)+" | "+" | ".join(["O" if value == -1 else 'X' if value == 1 else " " for value in self.gameBoard[i,:]]))
             print("_______________________________")
     
-    def showPyGameGameBoard(self):
+    def showPyGameGameBoard(self, my_font, background):
+        pg.font.init()
+        
 
-
-        background = pg.display.set_mode((540 ,540))
         background.fill("grey")
         pg.draw.rect(background, (0,102,0), (30,30,480,480))
 
         for i in range(30, 511, 60):
+
+            if (i-30)//60 != 8:
+                columnCoord = my_font.render(str((i-30)//60), False, (0, 0, 0))
+                lineCoord = my_font.render(str((i-30)//60), False, (0, 0, 0))
+                background.blit(columnCoord, (i+10 , 10))
+                background.blit(lineCoord, (10 , i+10))
+
             pg.draw.line(background , (255, 255, 255), (30, i), (510, i))
             pg.draw.line(background , (255, 255, 255), (i, 30), (i, 510))
-            pg.display.flip()
+
+        currentPlayer = my_font.render("Joueur actuel : noir" if self.player == 1 else "Joueur actuel : blanc", False, (0, 0, 0))
+        background.blit(currentPlayer, (540 , 50))
 
         for i in range(8):
             for j in range(8):
-                a = self.gameBoard[i, j] 
+                a = int(self.gameBoard[i, j]) 
 
                 if a == 1 : color = (0,0,0)
-                else : color = (255,255,255)
+                elif a == -1 : color = (255,255,255)
 
-                piece = Piece(color, (60*i, 60*j))
-                background.blit(piece.surf, piece.rect)
+                if a != 0:
+                    piece = Piece(color, (30 + 60*i +5, 30 + 60*j + 5))
+                    background.blit(piece.surf, piece.rect)
+
+        pg.display.flip()
 
 
-
+    def count(self):
+        return dict(zip(np.unique(self.gameBoard, return_counts = True)))
 
     def play(self):
 
         pg.init()
+
+        my_font = pg.font.SysFont('Comic Sans MS', 15, True)
+        background = pg.display.set_mode((740 ,540))
+
         running = True
         clock = pg.time.Clock()
         dt = 0
-        self.showPyGameGameBoard()
+        self.showPyGameGameBoard(my_font, background)
+        i = 0
 
-        while running:
+        while  running and i <= 60:
             
             
 
@@ -104,28 +127,33 @@ class Othello :
                 elif event.type == pg.MOUSEBUTTONUP:
                     position = pg.mouse.get_pos()
 
+                    position = ((position[0]-30)//60, (position[1]-30)//60)
+
                     if self.positionValid(position):
                         self.updateGameBoard(position)
+                        self.player *= -1
+                        i += 1
                         self.showPyGameGameBoard()
+                        
+                        
+        count = self.count()
+
+        my_font = pg.font.SysFont('Comic Sans MS', 50, True)
+
+        if count["1"] > count["-1"]:
+            victoryMessage = my_font.render("Les noirs gagnent !", False, (0, 0, 0))
+
+        elif count["1"] > count["-1"]:
+            victoryMessage = my_font.render("Les blancs gagnent !", False, (0, 0, 0))
+
+        elif count["1"] == count["-1"]:
+            victoryMessage = my_font.render("Match nul !", False, (0, 0, 0))
+
+        background.fill('grey')
+        background.blit(victoryMessage, (250,250))
 
         pg.quit()
             
-                
-            
-            # print("Joueur noir (X)" if self.player == 1 else "Joueur blanc (O)")
-            # print()
-            # self.showGameBoard()
-            # print()
-            
-            # positionStr = input("Saisissez la position (x,y) :")
-            # position = positionStr.replace("(", "").replace(")", "").split(",")
-            # while not self.positionValid(position):
-            #     positionStr = input("Saisissez la position (x,y) :")
-            #     position = positionStr.replace("(", "").replace(")", "").split(",")
-                
-            # self.updateGameBoard(position)
-            
-            # self.player *= -1
 
 class  Piece(pg.sprite.Sprite):
     def __init__(self, color, position):
@@ -134,9 +162,9 @@ class  Piece(pg.sprite.Sprite):
         self.x = position[0]
         self.y = position[1]
 
-        self.surf = pg.Surface((60,60))
+        self.surf = pg.Surface((50,50))
         self.surf.fill((0,102,0))
-        pg.draw.circle(self.surf, color, (30,30), 20)
+        pg.draw.circle(self.surf, color, (25,25), 20)
         self.rect = self.surf.get_rect(topleft = (self.x, self.y))
             
 def timer(fonction):
