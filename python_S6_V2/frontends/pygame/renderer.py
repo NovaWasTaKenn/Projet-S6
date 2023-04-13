bb9d55b54adf94be7b3625b9f36b68dddd0bbab5
NovaWasTaKenn <98171103+NovaWasTaKenn@users.noreply.github.com>
2023-04-13 20:19:36 +0200

    --

---
+++ python_S6_V2/frontends/pygame/renderer.py
@@ -0,0 +1,49 @@
+import pygame as pg
+
+from python_S6_V2.library.src.othello.game.renderers import Renderer
+from python_S6_V2.library.src.othello.logic.models import GameState, Pawn
+
+
+class PyGameRenderer(Renderer):
+    
+    def __init__(self):
+        
+        pg.init()
+
+        self.font = pg.font.SysFont('Comic Sans MS', 15, True)
+        self.background = pg.display.set_mode((740 ,540))
+
+
+    def Render(self, gameState : GameState) -> None:
+            pg.font.init()
+
+
+            self.background.fill("grey")
+            pg.draw.rect(self.background, (0,102,0), (30,30,480,480))
+
+            for i in range(30, 511, 60):
+
+                if (i-30)//60 != 8:
+                    columnCoord = self.font.render(str((i-30)//60), False, (0, 0, 0))
+                    lineCoord = self.font.render(str((i-30)//60), False, (0, 0, 0))
+                    self.background.blit(columnCoord, (i+10 , 10))
+                    self.background.blit(lineCoord, (10 , i+10))
+
+                pg.draw.line(self.background , (255, 255, 255), (30, i), (510, i))
+                pg.draw.line(self.background , (255, 255, 255), (i, 30), (i, 510))
+
+            currentPlayer = self.font.render("Joueur actuel : noir" if gameState.currentPawn == Pawn.BLACK else "Joueur actuel : blanc", False, (0, 0, 0))
+            self.background.blit(currentPlayer, (540 , 50))
+
+            for i in range(8):
+                for j in range(8):
+                    a = int(self.gameBoard[i, j]) 
+
+                    if a == 1 : color = (0,0,0)
+                    elif a == -1 : color = (255,255,255)
+
+                    if a != 0:
+                        piece = Piece(color, (30 + 60*i +5, 30 + 60*j + 5))
+                        self.background.blit(piece.surf, piece.rect)
+
+            pg.display.flip()
